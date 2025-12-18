94fed5996ca16a1aac2c37e814da68d6ce323721
202553024-web <202553024@std.umk.ac.id>
2025-12-18 21:11:38 +0700

    Update run.py

--- run.py
+++ run.py
@@ -438,12 +438,12 @@ for col, w in zip(["A", "B", "C", "D", "E"], [20, 18, 25, 20, 20]):
     ws_lr.append(["Total Beban", lr["Total Beban"]])
     ws_lr.append(["Laba / Rugi", lr["Laba/Rugi"]])
 
-    # =====================
-    # SIMPAN FILE
-    # =====================
-    wb.save(output)
-    output.seek(0)
-    return output.getvalue()
+        # =====================
+        # SIMPAN FILE
+        # =====================
+        wb.save(output)
+        output.seek(0)
+        return output.getvalue()
 
 # ============================
 # MENU NAVIGASI
@@ -973,6 +973,7 @@ st.markdown(
 """,
     unsafe_allow_html=True,
 )
+
 
 
 
